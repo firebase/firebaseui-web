ce5acf3efd1491d12f2dc16fd7d10f18aac3a455
Ti Wang <wti@google.com>
2018-06-13 20:26:27 -0700

    bump timeout for saucelab test

--- protractor.conf.js
+++ protractor.conf.js
@@ -45,10 +45,10 @@ config = {
   framework: 'jasmine',
   // The jasmine specs to run.
   specs: ['protractor_spec.js'],
-  // Jasmine options. Increase the timeout to 5min instead of the default 30s.
+  // Jasmine options. Increase the timeout to 20mins instead of the default 30s.
   jasmineNodeOpts: {
     // Default time to wait in ms before a test fails.
-    defaultTimeoutInterval: 5 * 60 * 1000
+    defaultTimeoutInterval: 20 * 60 * 1000
   }
 };
 
