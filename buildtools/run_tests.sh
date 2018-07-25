e111fed80ffafe8c5f5ec6c5806036b335e4fbd5
Ti Wang <wti@google.com>
2018-07-25 11:37:02 -0700

    disable

--- buildtools/run_tests.sh
+++ buildtools/run_tests.sh
@@ -70,12 +70,14 @@ trap killServer EXIT
 
 # If --saucelabs option is passed, forward it to the protractor command adding
 # the second argument that is required for local SauceLabs test run.
-if [[ $1 = "--saucelabs" ] && [[[$TRAVIS = true] && [$TRAVIS_SECURE_ENV_VARS = true]] || [$TRAVIS = false ]]]; then
-  seleniumStarted=false
-  sleep 2
-  echo "Using SauceLabs."
-  # $2 contains the tunnelIdentifier argument if specified, otherwise is empty.
-  $PROTRACTOR_BIN_PATH/protractor protractor.conf.js --saucelabs $2
+if [[ $1 = "--saucelabs" ]]; then
+  if [[[ "$TRAVIS" = true ] && [ "$TRAVIS_SECURE_ENV_VARS" = true ]] || [ "$TRAVIS" = false ]]; then
+    seleniumStarted=false
+    sleep 2
+    echo "Using SauceLabs."
+    # $2 contains the tunnelIdentifier argument if specified, otherwise is empty.
+    $PROTRACTOR_BIN_PATH/protractor protractor.conf.js --saucelabs $2
+  fi
 else
   echo "Using Headless Chrome."
   # Updates Selenium Webdriver.
