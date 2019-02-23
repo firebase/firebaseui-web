d72cf31bbcc84a117917b215eefad6dc6a6dd3a3
ics-kano <takeshi.kano@ics-web.jp>
2019-02-24 03:20:00 +0900

    build: change default locale to Japanese

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,10 @@ const TMP_DIR = 'out';
 const DEST_DIR = 'dist';
 
 // The locale that would be produced with no XTBs.
-const DEFAULT_LOCALE = 'en';
+// const DEFAULT_LOCALE = 'en';
+// modified 2019/02/24 by @ics-kano
+const DEFAULT_LOCALE = 'ja';
+// end of modify
 
 // The list of all locales that are supported.
 const ALL_LOCALES = ['ar-XB', 'ar', 'bg', 'ca', 'cs', 'da', 'de', 'el', 'en',
