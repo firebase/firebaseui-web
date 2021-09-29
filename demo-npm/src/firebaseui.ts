e546ea957eee8fa5b8ae2b845e35a4f51b36a8d0
James Daniels <jamesdaniels@google.com>
2021-09-28 21:48:50 -0400

    Flush out the webpack demo a bit more

--- demo-npm/src/firebaseui.ts
+++ demo-npm/src/firebaseui.ts
@@ -10,7 +10,48 @@ export const app = firebase.initializeApp(firebaseConfig);
 export const ui = new firebaseui.auth.AuthUI(app.auth());
 
 const clientId = undefined;
-const handleSignedInUser = console.log;
+
+const handleSignedInUser = (user: firebase.User) => {
+  document.getElementById('user-signed-in')!.style.display = 'block';
+  document.getElementById('user-signed-out')!.style.display = 'none';
+  document.getElementById('name')!.textContent = user.displayName;
+  document.getElementById('email')!.textContent = user.email;
+  document.getElementById('phone')!.textContent = user.phoneNumber;
+  if (user.photoURL) {
+    var photoURL = user.photoURL;
+    // Append size to the photo URL for Google hosted images to avoid requesting
+    // the image with its original resolution (using more bandwidth than needed)
+    // when it is going to be presented in smaller size.
+    if ((photoURL.indexOf('googleusercontent.com') != -1) ||
+        (photoURL.indexOf('ggpht.com') != -1)) {
+      photoURL = photoURL + '?sz=' +
+          document.getElementById('photo')!.clientHeight;
+    }
+    // TODO typing for the img element
+    (document.getElementById('photo')! as any).src = photoURL;
+    document.getElementById('photo')!.style.display = 'block';
+  } else {
+    document.getElementById('photo')!.style.display = 'none';
+  }
+}
+
+// Listen to change in auth state so it displays the correct UI for when
+// the user is signed in or not.
+firebase.auth().onAuthStateChanged(function(user) {
+  document.getElementById('loading')!.style.display = 'none';
+  document.getElementById('loaded')!.style.display = 'block';
+  user ? handleSignedInUser(user) : handleSignedOutUser();
+});
+
+/**
+ * Displays the UI for a signed out user.
+ */
+ var handleSignedOutUser = function() {
+  document.getElementById('user-signed-in')!.style.display = 'none';
+  document.getElementById('user-signed-out')!.style.display = 'block';
+  ui.start('#firebaseui-container', config);
+};
+
 const getRecaptchaMode = () => 'invisible';
 const getEmailSignInMethod = () => 'password';
 const getDisableSignUpStatus = () => false;
@@ -83,3 +124,21 @@ export const config: firebaseui.auth.Config = {
         firebaseui.auth.CredentialHelper.GOOGLE_YOLO :
         firebaseui.auth.CredentialHelper.NONE
 };
+
+
+/**
+ * Deletes the user's account.
+ */
+export const deleteAccount = () => app.auth().currentUser!.delete().catch(async error => {
+  if (error.code == 'auth/requires-recent-login') {
+    // The user's credential is too old. She needs to sign in again.
+    await app.auth().signOut();
+    // The timeout allows the message to be displayed after the UI has
+    // changed to the signed out state.
+    setTimeout(function() {
+      alert('Please sign in again to delete your account.');
+    }, 1);
+  }
+});
+
+export const signOut = () => app.auth().signOut();
--- demo-npm/src/index.ts
+++ demo-npm/src/index.ts
@@ -1,5 +1,8 @@
-const { ui, config, app } = await import('./firebaseui');
+const { ui, config, deleteAccount, signOut } = await import('./firebaseui');
 
 ui.start('#firebaseui-container', config);
+
+document.getElementById('delete-account')!.addEventListener('click', () => deleteAccount());
+document.getElementById('sign-out')!.addEventListener('click', () => signOut());
 
 export { };
