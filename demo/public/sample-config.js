8336d16b834b11dbadbbbf1903a7a5da7c69c74b
James Daniels <jamesdaniels@google.com>
2021-10-08 16:23:02 -0400

    Revert sample-config

--- demo/public/sample-config.js
+++ demo/public/sample-config.js
@@ -12,6 +12,16 @@
  * limitations under the License.
  */
 
+var config = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  databaseURL: "https://your-app.firebaseio.com",
+  storageBucket: "your-app.appspot.com",
+};
+firebase.initializeApp(config);
+
+
 // Google OAuth Client ID, needed to support One-tap sign-up.
 // Set to null if One-tap sign-up is not supported.
-var CLIENT_ID = 'YOUR_OAUTH_CLIENT_ID';
+var CLIENT_ID =
+    'YOUR_OAUTH_CLIENT_ID';
