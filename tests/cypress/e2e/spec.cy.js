5883627e13cef660b86b05edd4903f49c543a84e
andreaowu <andreaowu@berkeley.edu>
2023-06-21 18:54:38 -0700

    change back to percy

--- tests/cypress/e2e/spec.cy.js
+++ tests/cypress/e2e/spec.cy.js
@@ -2,6 +2,6 @@ describe('Firebase UI for web', () => {
   it('loads web example app', () => {
     cy.visit('https://fir-ui-demo-84a6c.firebaseapp.com/');
     cy.wait(2000);
-    cy.screenshot();
+    cy.percySnapshot();
   })
 })
