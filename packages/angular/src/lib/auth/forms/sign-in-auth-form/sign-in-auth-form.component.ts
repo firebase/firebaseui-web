d7815f324d0f29a51a01fe9588a25f12492f3913
Elliot Hesp <elliot.hesp@gmail.com>
2025-09-18 20:58:38 +0100

    refactor(angular): Upgrade tanstack form

--- packages/angular/src/lib/auth/forms/sign-in-auth-form/sign-in-auth-form.component.ts
+++ packages/angular/src/lib/auth/forms/sign-in-auth-form/sign-in-auth-form.component.ts
@@ -14,91 +14,68 @@
  * limitations under the License.
  */
 
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Output, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { injectForm, TanStackField } from "@tanstack/angular-form";
+import { injectForm, TanStackField, TanStackAppField } from "@tanstack/angular-form";
 import { injectSignInAuthFormSchema, injectTranslation, injectUI } from "../../../provider";
-import { ButtonComponent } from "../../../components/button/button.component";
 import { TermsAndPrivacyComponent } from "../../../components/terms-and-privacy/terms-and-privacy.component";
 import { FirebaseUIError, signInWithEmailAndPassword } from "@firebase-ui/core";
+
+import {
+  FormInputComponent,
+  FormSubmitComponent,
+  FormErrorMessageComponent,
+} from "../../../components/form/form.component";
+import { UserCredential } from "firebase/auth";
 
 @Component({
   selector: "fui-sign-in-auth-form",
   standalone: true,
-  imports: [CommonModule, TanStackField, ButtonComponent, TermsAndPrivacyComponent],
+  imports: [
+    CommonModule,
+    TanStackField,
+    TanStackAppField,
+    TermsAndPrivacyComponent,
+    FormInputComponent,
+    FormSubmitComponent,
+    FormErrorMessageComponent,
+  ],
   template: `
     <form (submit)="handleSubmit($event)" class="fui-form">
       <fieldset>
-        <ng-container [tanstackField]="form" name="email" #email="field">
-          <label [for]="email.api.name">
-            <span>{{ emailLabel | async }}</span>
-            <input
-              type="email"
-              [id]="email.api.name"
-              [name]="email.api.name"
-              [value]="email.api.state.value"
-              (blur)="email.api.handleBlur()"
-              (input)="email.api.handleChange($any($event).target.value)"
-              [attr.aria-invalid]="!!email.api.state.meta.errors.length"
-            />
-            <span role="alert" aria-live="polite" class="fui-form__error" *ngIf="!!email.api.state.meta.errors.length">
-              {{ email.api.state.meta.errors.join(", ") }}
-            </span>
-          </label>
-        </ng-container>
+        <fui-form-input
+          name="email"
+          tanstack-app-field
+          [tanstackField]="form"
+          label="Email Label TODO"
+        ></fui-form-input>
       </fieldset>
       <fieldset>
-        <ng-container [tanstackField]="form" name="password" #password="field">
-          <label [for]="password.api.name">
-            <span class="flex">
-              <span class="flex-grow">{{ passwordLabel() }}</span>
-              @if(forgotPassword) {
-                <button type="button" (click)="forgotPassword.emit()" class="fui-form__action">
-                  {{ forgotPasswordLabel() }}
-                </button>
-              }
-            </span>
-            <input
-              type="password"
-              [id]="password.api.name"
-              [name]="password.api.name"
-              [value]="password.api.state.value"
-              (blur)="password.api.handleBlur()"
-              (input)="password.api.handleChange($any($event).target.value)"
-              [attr.aria-invalid]="!!password.api.state.meta.errors.length"
-            />
-            <span
-              role="alert"
-              aria-live="polite"
-              class="fui-form__error"
-              *ngIf="!!password.api.state.meta.errors.length"
-            >
-              {{ password.api.state.meta.errors.join(", ") }}
-            </span>
-          </label>
-        </ng-container>
+        <fui-form-input name="password" tanstack-app-field [tanstackField]="form" label="Password Label TODO">
+          @if (forgotPassword) {
+            <button fui-form-action (click)="forgotPassword.emit()">
+              {{ forgotPasswordLabel() }}
+            </button>
+          }
+        </fui-form-input>
       </fieldset>
 
       <fui-terms-and-privacy></fui-terms-and-privacy>
 
       <fieldset>
-        <button fui-button type="submit">
+        <fui-form-submit>
           {{ signInLabel() }}
-        </button>
-        <div class="fui-form__error" *ngIf="formError">{{ formError }}</div>
+        </fui-form-submit>
+        <fui-form-error-message></fui-form-error-message>
       </fieldset>
 
-      @if(register) {
-        <div class="flex justify-center items-center">
-          <button type="button" (click)="register.emit()" class="fui-form__action">
-            {{ noAccountLabel() }} {{ registerLabel() }}
-          </button>
-        </div>
+      @if (register) {
+        <button fui-form-action (click)="register.emit()">{{ noAccountLabel() }} {{ registerLabel() }}</button>
       }
     </form>
   `,
 })
-export class SignInAuthFormComponent {
+export class SignInAuthFormComponent implements OnInit {
   private ui = injectUI();
   private formSchema = injectSignInAuthFormSchema();
 
@@ -112,6 +89,7 @@ export class SignInAuthFormComponent {
 
   @Output() forgotPassword = new EventEmitter<void>();
   @Output() register = new EventEmitter<void>();
+  @Output() signIn?: EventEmitter<UserCredential>;
 
   formError: string | null = null;
 
@@ -120,59 +98,32 @@ export class SignInAuthFormComponent {
       email: "",
       password: "",
     },
-    validators: {
-      onBlur: this.formSchema(),
-      onSubmit: this.formSchema(),
-    },
-  }) as any; // TODO(ehesp): Fix this - types go too deep
+  });
 
-  async handleSubmit(event: SubmitEvent) {
-    event.preventDefault();
-    event.stopPropagation();
-
-    const email = this.form.state.values.email;
-    const password = this.form.state.values.password;
-
-    if (!email || !password) {
-      return;
-    }
-
-    await this.validateAndSignIn(email, password);
+  handleSubmit(event: SubmitEvent) {
+    event.preventDefault()
+    event.stopPropagation()
+    this.form.handleSubmit()
   }
 
-  async validateAndSignIn(email: string, password: string) {
-    try {
-      const validationResult = this.formSchema().safeParse({
-        email,
-        password,
-      });
+  ngOnInit() {
+    this.form.update({
+      validators: {
+        onBlur: this.formSchema(),
+        onSubmit: this.formSchema(),
+        onSubmitAsync: async ({ value }) => {
+          try {
+            const credential = await signInWithEmailAndPassword(this.ui(), value.email, value.password);
+            this.signIn?.emit(credential);
+          } catch (error) {
+            if (error instanceof FirebaseUIError) {
+              return error.message;
+            }
 
-      if (!validationResult.success) {
-        const validationErrors = validationResult.error.format();
-
-        if (validationErrors.email?._errors?.length) {
-          this.formError = validationErrors.email._errors[0];
-          return;
-        }
-
-        if (validationErrors.password?._errors?.length) {
-          this.formError = validationErrors.password._errors[0];
-          return;
-        }
-
-        this.formError = this.unknownErrorLabel();
-        return;
-      }
-
-      this.formError = null;
-      await signInWithEmailAndPassword(this.ui(), email, password);
-    } catch (error) {
-      if (error instanceof FirebaseUIError) {
-        this.formError = error.message;
-        return;
-      }
-
-      this.formError = this.unknownErrorLabel();
-    }
+            return this.unknownErrorLabel();
+          }
+        },
+      },
+    });
   }
 }
