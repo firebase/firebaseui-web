b434354e4c0c2c1d2ed6a1b26eb4a10bd8f2d3c3
Elliot Hesp <elliot.hesp@gmail.com>
2025-10-01 11:45:52 +0100

    feat(core): Add registerFramework util

---
+++ packages/core/src/register-framework.ts
@@ -0,0 +1,11 @@
+import { registerVersion } from "firebase/app";
+
+/**
+ * Register a framework with the FirebaseUI configuration.
+ * @internal
+ * @param framework The type of framework being registered.
+ * @param version The version of the framework being registered.
+ */
+export function registerFramework(framework: string, version: string) {
+  registerVersion("firebase-ui", version, framework);
+}
