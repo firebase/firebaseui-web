05125f7a1aaebca0470df3ae0a23d763c7a92649
Elliot Hesp <elliot.hesp@gmail.com>
2025-10-07 19:20:12 +0100

    feat(shadcn): Forgot password components

---
+++ packages/shadcn/src/registry/forgot-password-auth-screen.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useUI, ForgotPasswordAuthScreenProps } from "@firebase-ui/react";
+import { getTranslation } from "@firebase-ui/core";
+
+import { ForgotPasswordAuthForm } from "@/registry/forgot-password-auth-form";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+
+export type { ForgotPasswordAuthScreenProps };
+
+export function ForgotPasswordAuthScreen(props: ForgotPasswordAuthScreenProps) {
+  const ui = useUI();
+
+  const titleText = getTranslation(ui, "labels", "resetPassword");
+  const subtitleText = getTranslation(ui, "prompts", "enterEmailToReset");
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{titleText}</CardTitle>
+        <CardDescription>{subtitleText}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ForgotPasswordAuthForm {...props} />
+      </CardContent>
+    </Card>
+  );
+}
