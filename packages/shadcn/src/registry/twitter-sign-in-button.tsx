ea958f64e65bfd3b7041eeb960182bf1c499fa6c
Elliot Hesp <elliot.hesp@gmail.com>
2025-10-08 11:42:35 +0100

    feat(shadcn): Twitter sign in button

---
+++ packages/shadcn/src/registry/twitter-sign-in-button.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { TwitterAuthProvider } from "firebase/auth";
+import { getTranslation } from "@firebase-ui/core";
+import { useUI, TwitterSignInButtonProps, TwitterLogo } from "@firebase-ui/react";
+
+import { OAuthButton } from "@/registry/oauth-button";
+
+export type { TwitterSignInButtonProps };
+
+export function TwitterSignInButton({ provider, themed }: TwitterSignInButtonProps) {
+  const ui = useUI();
+
+  return (
+    <OAuthButton provider={provider || new TwitterAuthProvider()} themed={themed}>
+      <TwitterLogo />
+      <span>{getTranslation(ui, "labels", "signInWithTwitter")}</span>
+    </OAuthButton>
+  );
+}
