2d6882ebcace10da8927fe6ce8c18e60a0b25a55
Elliot Hesp <elliot.hesp@gmail.com>
2025-10-27 11:05:46 +0000

    feat(react): MFA TOTP assertion flow

--- packages/react/src/auth/forms/multi-factor-auth-assertion-form.tsx
+++ packages/react/src/auth/forms/multi-factor-auth-assertion-form.tsx
@@ -15,17 +15,17 @@ export function MultiFactorAuthAssertionForm() {
   }
 
   // If only a single hint is provided, select it by default to improve UX.
-  const [factor, setFactor] = useState<MultiFactorInfo | undefined>(
+  const [hint, setHint] = useState<MultiFactorInfo | undefined>(
     resolver.hints.length === 1 ? resolver.hints[0] : undefined
   );
 
-  if (factor) {
-    if (factor.factorId === PhoneMultiFactorGenerator.FACTOR_ID) {
-      return <SmsMultiFactorAssertionForm hint={factor} />;
+  if (hint) {
+    if (hint.factorId === PhoneMultiFactorGenerator.FACTOR_ID) {
+      return <SmsMultiFactorAssertionForm hint={hint} />;
     }
 
-    if (factor.factorId === TotpMultiFactorGenerator.FACTOR_ID) {
-      return <TotpMultiFactorAssertionForm />;
+    if (hint.factorId === TotpMultiFactorGenerator.FACTOR_ID) {
+      return <TotpMultiFactorAssertionForm hint={hint} />;
     }
   }
 
@@ -34,11 +34,11 @@ export function MultiFactorAuthAssertionForm() {
       <p>TODO: Select a multi-factor authentication method</p>
       {resolver.hints.map((hint) => {
         if (hint.factorId === TotpMultiFactorGenerator.FACTOR_ID) {
-          return <TotpButton key={hint.factorId} onClick={() => setFactor(hint)} />;
+          return <TotpButton key={hint.factorId} onClick={() => setHint(hint)} />;
         }
 
         if (hint.factorId === PhoneMultiFactorGenerator.FACTOR_ID) {
-          return <SmsButton key={hint.factorId} onClick={() => setFactor(hint)} />;
+          return <SmsButton key={hint.factorId} onClick={() => setHint(hint)} />;
         }
 
         return null;
