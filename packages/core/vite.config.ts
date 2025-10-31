ecfce260bb5aac5c365e0d70494beb3ff74edb13
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2025-10-31 11:08:31 -0400

    lint

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -7,7 +7,10 @@ export default defineConfig({
   resolve: {
     alias: {
       "@firebase-oss/ui-styles": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "../styles/src"),
-      "@firebase-oss/ui-translations": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "../translations/src"),
+      "@firebase-oss/ui-translations": path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        "../translations/src"
+      ),
       "~/tests": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./tests"),
       "~": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./src"),
     },
