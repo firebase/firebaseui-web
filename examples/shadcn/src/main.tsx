93142455688bc33bc11a328d12577c45d92526a6
Darren Ackers <ackers86@hotmail.com>
2025-11-04 12:50:43 +0000

    chore(shadn): updated shadcn app to include shadcn components

--- examples/shadcn/src/main.tsx
+++ examples/shadcn/src/main.tsx
@@ -4,6 +4,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -13,48 +14,36 @@
  * limitations under the License.
  */
 
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-import { FirebaseUIProvider } from "@invertase/firebaseui-react";
 import ReactDOM from "react-dom/client";
-
-import { ui } from "./firebase/firebase";
-
+import { FirebaseUIProvider } from "@invertase/firebaseui-react";
+import { ui, auth } from "./firebase/firebase";
 import App from "./App";
-import { Header } from "./components/header";
-
-/** Sign In */
-import { EmailLinkAuthScreen } from "@/components/email-link-auth-screen";
-import { ForgotPasswordAuthScreen } from "@/components/forgot-password-auth-screen";
-import { SignInAuthScreen } from "@/components/sign-in-auth-screen";
-import { SignUpAuthScreen } from "@/components/sign-up-auth-screen";
+import { hiddenRoutes, routes } from "./routes";
 
 const root = document.getElementById("root")!;
 
-ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Header />
-    <FirebaseUIProvider
-      ui={ui}
-      policies={{
-        termsOfServiceUrl: "https://www.google.com",
-        privacyPolicyUrl: "https://www.google.com",
-      }}
-    >
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/screens/sign-in-auth-screen" element={<SignInAuthScreen />} />
-        {/* <Route path="/screens/sign-in-auth-screen-w-handlers" element={<SignInAuthScreenWithHandlers />} /> */}
-        {/* <Route path="/screens/sign-in-auth-screen-w-oauth" element={<SignInAuthScreenWithOAuth />} /> */}
-        <Route path="/screens/email-link-auth-screen" element={<EmailLinkAuthScreen />} />
-        {/* <Route path="/screens/email-link-auth-screen-w-oauth" element={<EmailLinkAuthScreenWithOAuth />} /> */}
-        {/* <Route path="/screens/phone-auth-screen" element={<PhoneAuthScreen />} /> */}
-        {/* <Route path="/screens/phone-auth-screen-w-oauth" element={<PhoneAuthScreenWithOAuth />} /> */}
-        <Route path="/screens/sign-up-auth-screen" element={<SignUpAuthScreen />} />
-        {/* <Route path="/screens/sign-up-auth-screen-w-oauth" element={<SignUpAuthScreenWithOAuth />} /> */}
-        {/* <Route path="/screens/oauth-screen" element={<OAuthScreen />} /> */}
-        <Route path="/screens/forgot-password-screen" element={<ForgotPasswordAuthScreen />} />
-      </Routes>
-    </FirebaseUIProvider>
-  </BrowserRouter>
-);
+const allRoutes = [...routes, ...hiddenRoutes];
+
+// Hacky way to ensure we have an auth state before showing the app...
+auth.authStateReady().then(() => {
+  ReactDOM.createRoot(root).render(
+    <BrowserRouter>
+      <FirebaseUIProvider
+        ui={ui}
+        policies={{
+          termsOfServiceUrl: "https://www.google.com",
+          privacyPolicyUrl: "https://www.google.com",
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<App />} />
+          {allRoutes.map((route) => (
+            <Route key={route.path} path={route.path} element={<route.component />} />
+          ))}
+        </Routes>
+      </FirebaseUIProvider>
+    </BrowserRouter>
+  );
+});
