c2182fb2a425dbc108b21305816b85d0459510d2
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2025-11-24 09:52:21 -0500

    set version and tag dynamically based on tag or commit

---
+++ publish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# This script is run by each package in the packages/ folder
+
+# Configure npm authentication
+# NPM_TOKEN is set in cloudbuild.yaml
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >.npmrc
+
+# Decide the correct version and npm tag.
+# Uses default substitutions from https://docs.cloud.google.com/build/docs/configuring-builds/substitute-variable-values#using_default_substitutions
+if [ -n "$TAG_NAME" ]; then
+	echo "Git Tag detected: $TAG_NAME"
+	NPM_TAG=beta
+	# Delete the 'v' from the tag to get the version to publish
+	OVERRIDE_VERSION=${TAG_NAME/v/}
+else
+	echo "No Git Tag detected (Commit trigger)"
+	NPM_TAG=exp
+	OVERRIDE_VERSION=$(node -e "console.log(require('./package.json').version)")-exp.$SHORT_SHA
+fi
+
+echo "Publishing version $OVERRIDE_VERSION with npm tag '$NPM_TAG' for commit $COMMIT_SHA."
+npm --no-git-tag-version --allow-same-version version $OVERRIDE_VERSION
+npm publish --tag $NPM_TAG --dry-run
