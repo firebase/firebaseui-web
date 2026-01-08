56fa5a0183c10de304941210a5c201f04174dca5
MichaelVerdon <mwverdon2000@gmail.com>
2026-01-08 14:42:06 +0000

    feat(auth): Add YahooSignInButton support

--- packages/react/src/auth/index.ts
+++ packages/react/src/auth/index.ts
@@ -128,4 +128,5 @@ export {
   type MicrosoftSignInButtonProps,
 } from "./oauth/microsoft-sign-in-button";
 export { TwitterSignInButton, TwitterLogo, type TwitterSignInButtonProps } from "./oauth/twitter-sign-in-button";
+export { YahooSignInButton, YahooLogo, type YahooSignInButtonProps } from "./oauth/yahoo-sign-in-button";
 export { OAuthButton, useSignInWithProvider, type OAuthButtonProps } from "./oauth/oauth-button";
