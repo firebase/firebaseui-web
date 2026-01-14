da8e9123159fb1bdf0d42c51771dcc9634a40dd9
Darren Ackers <ackers86@hotmail.com>
2026-01-14 14:09:24 +0000

    fix(react): updated next example routes with the new Yahoo logo

--- examples/nextjs/app/screens/phone-auth-screen-w-oauth/page.tsx
+++ examples/nextjs/app/screens/phone-auth-screen-w-oauth/page.tsx
@@ -24,6 +24,7 @@ import {
   PhoneAuthScreen,
   TwitterSignInButton,
   MicrosoftSignInButton,
+  YahooSignInButton,
   OAuthButton,
 } from "@firebase-oss/ui-react";
 import { OAuthProvider } from "firebase/auth";
@@ -45,6 +46,7 @@ export default function PhoneAuthScreenWithOAuthPage() {
       <GitHubSignInButton />
       <MicrosoftSignInButton />
       <TwitterSignInButton />
+      <YahooSignInButton />
       <LineSignInButton />
     </PhoneAuthScreen>
   );
