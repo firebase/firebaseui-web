af263427063668cc740cb30348a62533b5867c0c
Darren Ackers <ackers86@hotmail.com>
2026-01-14 14:08:36 +0000

    fix(react): updated example routes with the new Yahoo logo

--- examples/react/src/screens/phone-auth-screen-w-oauth.tsx
+++ examples/react/src/screens/phone-auth-screen-w-oauth.tsx
@@ -24,6 +24,7 @@ import {
   PhoneAuthScreen,
   TwitterSignInButton,
   MicrosoftSignInButton,
+  YahooSignInButton,
 } from "@firebase-oss/ui-react";
 import { useNavigate } from "react-router";
 
@@ -42,6 +43,7 @@ export default function PhoneAuthScreenWithOAuthPage() {
       <GitHubSignInButton />
       <MicrosoftSignInButton />
       <TwitterSignInButton />
+      <YahooSignInButton />
     </PhoneAuthScreen>
   );
 }
