9a6ce506282cd6f72631e37acee368bb5332418d
russellwheatley <russellwheatley85@gmail.com>
2026-03-06 15:05:13 +0000

    chore: fix type declaration issue

--- examples/vue/src/env.d.ts
+++ examples/vue/src/env.d.ts
@@ -1,3 +1,9 @@
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent;
+  export default component;
+}
+
 declare module "*.jsx" {
   const component: unknown;
   export default component;
